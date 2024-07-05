0f1b932ef4c8cb6ee9bd9694596a158bfc5d1243
DESKTOP-O7B7RVF\Patricia <jbrsistemas@gmail.com>
2024-07-04 22:51:39 -0500

    Añadimos la redirección a la vista resumenorden

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -117,4 +117,9 @@ public class HomeController {
         model.addAttribute("orden", orden);
         return "/usuario/carrito";
     }
+
+    @GetMapping("/order")
+    public String order(){
+        return "usuario/resumenorden";
+    }
 }
